9d1c279c2704fb5ff4f468ff18d68a175e4ec094
Genek <Gene@Yevgens-MacBook-Pro.local>
2017-04-12 22:36:27 +0300

    initial commit

---
+++ TestC/TestC/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  TestC
+//
+//  Created by Gene on 4/12/17.
+//  Copyright © 2017 IDAP. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
